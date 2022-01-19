3f46ee2f040bdc529dca8dd7ac3968a0b5ca413c
Renan <renancossenzo@hotmail.com>
2022-01-19 18:33:05 -0300

    Criando interface de repositório para cep

---
+++ src/Dominio/Repositorios/ICepRepositorio.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dominio.Entidades;
+
+namespace Dominio.Repositorios
+{
+    public interface ICepRepositorio
+    {
+        Task<IEnumerable<Cep>> ObterCeps();
+    }
+}
