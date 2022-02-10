a72a0ac1e8b89818862dd06b41b956881b1c295f
Renan <renancossenzo@hotmail.com>
2022-02-10 20:50:44 -0300

    Adicionando propriedade de tamanho da lista

--- src/Api/Models/RetornoErroModel.cs
+++ src/Api/Models/RetornoErroModel.cs
@@ -1,14 +1,17 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Api.Models
 {
     public class RetornoErroModel
     {
         public IEnumerable<string> Erros { get; }
+        public int Tamanho { get; }
 
         public RetornoErroModel(IEnumerable<string> erros)
         {
             Erros = erros;
+            Tamanho = erros.Count();
         }
     }
 }
